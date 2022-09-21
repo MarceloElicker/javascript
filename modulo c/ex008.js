cfe0d2833f29259ef3d346292261643a2dc564fb
MarceloElicker <marceloelickerjobs@gmail.com>
2022-09-20 22:50:45 -0300

    Condições

---
+++ modulo c/ex008.js
@@ -0,0 +1,11 @@
+
+var vel = 61;
+
+console.log('Sua velocidade é de ' + vel + ' Km/h')
+
+if(vel > 60){
+
+    console.log('Velocidade max ultrapassada, vc foi multado!');
+}
+
+console.log('use sinto de segurança')
---
+++ modulo c/ex009.js
@@ -0,0 +1,12 @@
+
+var país = 'EUA';
+
+if(país == 'Brasil'){
+
+    console.log('Brasileiro');
+
+}else{
+    console.log('Estrangeiro')
+}
+
+console.log(`seja bem vindo ${país}`)
