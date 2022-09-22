e0cf517c2c28302bb2cb4f96ea8b852b6dff58ec
MarceloElicker <marceloelickerjobs@gmail.com>
2022-09-22 14:38:44 -0300

    Variáveis

---
+++ modulo E/repetiçoes.js
@@ -0,0 +1,18 @@
+var c = 1;
+/*
+while (c <=6){
+    console.log(`Tudo bem? ${c}`);
+    c++;
+}
+
+
+do{
+    console.log(`Tudo bem? ${c}`);
+    c++;
+}while (c <=10)
+
+*/
+
+for (var c=1; c<=6; c++){
+    console.log(`Tudo bem? ${c}`);
+}
---
+++ modulo F/array.js
@@ -0,0 +1,12 @@
+let a =[5,8,4]
+
+console.log(a.sort())
+
+console.log(a)
+
+a.push(10)
+
+console.log(a)
+
+console.log(a.length)
+
