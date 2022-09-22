2bafe328b348a3c24e86ad23a049f1e5b9a23b73
MarceloElicker <marceloelickerjobs@gmail.com>
2022-09-22 00:00:51 -0300

    projeto relógio

---
+++ modulo D/ex012.js
@@ -0,0 +1,12 @@
+var idade = 18;
+
+if(idade < 16){
+    console.log("Não vota");
+}else{
+    if(idade <= 17){
+        console.log("Menor de idade, mas pode votar!");
+    }else{
+        console.log("Maior de idade!");
+    }
+    
+}
